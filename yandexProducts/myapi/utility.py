836835c2ba0347f9968aed43667d3e60e3125683
Zalozhnyy <niczz9797@gmail.com>
2022-06-21 10:20:13 +0300

    перенос логики из view

--- yandexProducts/myapi/utility.py
+++ yandexProducts/myapi/utility.py
@@ -1,11 +1,14 @@
-from typing import List, Set
+import datetime
+from typing import List, Set, Union
 
 from django.db import connection, reset_queries
 import time
 import functools
 
+from django.db.models import QuerySet, Sum, Max
+
 from .models import ItemModel
-from .serializers import PriceHistorySerializer, ItemSerializer
+from .serializers import PriceHistorySerializer, ItemSerializer, save_history
 
 
 def query_debugger(func):
@@ -36,3 +39,55 @@ def import_handler(item: dict):
 
     if serializer.is_valid(raise_exception=True):
         serializer.save()
+
+
+def calculate_category_prices(children: Union[QuerySet, List[ItemModel]]):
+    calculated = set()
+    for child in children:
+        if not child.parentId or child.parentId in calculated:
+            continue
+
+        parent = child.parentId
+
+        all_children = ItemModel.objects.filter(parentId=child.parentId).aggregate(
+            price=Sum("price_info__items_price_count"),
+            count=Sum("price_info__category_items_count"),
+            date=Max("date")
+        )
+        parent.price_info.items_price_count = all_children['price']
+        parent.price_info.category_items_count = all_children['count']
+        parent.price = parent.price_info.items_price_count // parent.price_info.category_items_count
+        parent.price_info.save()
+
+        parent.date = all_children['date']
+        parent.save()
+
+        save_history(parent.id, parent.price, parent.date)
+
+        calculated.add(parent)
+
+    if calculated:
+        calculate_category_prices(list(calculated))
+
+
+def decrement_deleted_item(instance: ItemModel):
+    count = instance.price_info.category_items_count
+    price = instance.price_info.items_price_count
+    date = datetime.datetime.now()
+
+    while instance.parentId:
+        instance = instance.parentId
+
+        instance.price_info.category_items_count -= count
+        instance.price_info.items_price_count -= price
+        instance.price_info.save()
+
+        if instance.price_info.category_items_count > 0:
+            new_price = instance.price_info.items_price_count // instance.price_info.category_items_count
+        else:
+            new_price = None
+        instance.price = new_price
+
+        instance.date = date
+        instance.save()
+        save_history(instance.id, instance.price, instance.date)
--- yandexProducts/myapi/views.py
+++ yandexProducts/myapi/views.py
@@ -23,58 +23,6 @@ item_not_found = lambda: JsonResponse({"code": status.HTTP_404_NOT_FOUND, "messa
                                       )
 
 
-def calculate_category_prices(children: Union[QuerySet, List[ItemModel]]):
-    calculated = set()
-    for child in children:
-        if not child.parentId or child.parentId in calculated:
-            continue
-
-        parent = child.parentId
-
-        all_children = ItemModel.objects.filter(parentId=child.parentId).aggregate(
-            price=Sum("price_info__items_price_count"),
-            count=Sum("price_info__category_items_count"),
-            date=Max("date")
-        )
-        parent.price_info.items_price_count = all_children['price']
-        parent.price_info.category_items_count = all_children['count']
-        parent.price = parent.price_info.items_price_count // parent.price_info.category_items_count
-        parent.price_info.save()
-
-        parent.date = all_children['date']
-        parent.save()
-
-        save_history(parent.id, parent.price, parent.date)
-
-        calculated.add(parent)
-
-    if calculated:
-        calculate_category_prices(list(calculated))
-
-
-def decrement_deleted_item(instance: ItemModel):
-    count = instance.price_info.category_items_count
-    price = instance.price_info.items_price_count
-    date = datetime.datetime.now()
-
-    while instance.parentId:
-        instance = instance.parentId
-
-        instance.price_info.category_items_count -= count
-        instance.price_info.items_price_count -= price
-        instance.price_info.save()
-
-        if instance.price_info.category_items_count > 0:
-            new_price = instance.price_info.items_price_count // instance.price_info.category_items_count
-        else:
-            new_price = None
-        instance.price = new_price
-
-        instance.date = date
-        instance.save()
-        save_history(instance.id, instance.price, instance.date)
-
-
 class ItemAPIView(ListCreateAPIView):
 
     def get(self, request, *args, **kwargs):
